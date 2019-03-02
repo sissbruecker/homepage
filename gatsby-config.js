33ba214f2359fc0868dfdfcb3956cd13efd31f0b
Sascha Ißbrücker <sascha.issbruecker@byteagenten.de>
2019-03-02 13:59:23 +0100

    Cleanup and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Sascha Ißbrücker`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `This is a website for showcasing my professional resume and portfolio.`,
+    author: `Sascha Ißbrücker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,14 +21,14 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2f89fc`,
+        theme_color: `#2f89fc`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
